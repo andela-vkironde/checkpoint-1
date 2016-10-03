568d64b28227bfe3063a96d5055e3016d7c432cb
andela-vkironde <victor.kironde@andela.com>
2016-10-03 17:48:42 +0300

    Create MusicImporter

--- lib/Models/Song.rb
+++ lib/Models/Song.rb
@@ -31,5 +31,15 @@ class Song
     @genre = value
     @genre.songs.push(self) unless @genre.songs.include? self
   end
-  
+  def self.new_from_filename(filename)
+    filename = filename.chomp(".mp3")
+    song_info = filename.split(" - ")
+    songArtist = Artist.find_or_create_by_name(song_info[0])
+    songGenre = Genre.find_or_create_by_name(song_info[2])
+
+    self.new(song_info[1], songArtist, songGenre)
+  end
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
 end
