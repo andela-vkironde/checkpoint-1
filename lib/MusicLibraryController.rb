5a275ff7a17cfce6099b99b4991e9208740b259c
andela-vkironde <victor.kironde@andela.com>
2016-10-11 11:20:28 +0300

    Allow a user to list Songs, Artists and Genres

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,15 +1,50 @@
 class MusicLibraryController
-  attr_accessor :path
-  def initialize(path = './db/mp3s')
+  attr_accessor :path, :importer
+  def initialize(path = "./db/mp3s")
     @path = path
     importer = MusicImporter.new(@path)
     importer.import
   end
+
   def call
-    self
-    puts "Options:"
-    choice = gets.chomp
-  end
-
-
+    loop do
+      puts "Options:"
+      choice = gets.chomp
+      case choice
+      when "list songs"
+        Song.all.each_with_index do |song, index|
+          puts "#{index += 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
+      when "list artists"
+        Song.all.each_with_index do |song, index|
+          puts "#{index += 1}. #{song.artist.name}"
+        end
+      when "list genres"
+        Song.all.each_with_index do |song, index|
+          puts "#{index += 1}. #{song.genre.name}"
+        end
+      when "list artist"
+        puts "Enter an artist"
+        artist = Artist.find_by_name(gets.chomp)
+        if artist
+        artist.songs .each do |song|
+          puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
+        else
+          puts "artist not found"
+      end
+    when "list genre"
+      puts "Enter a genre"
+      genre = Genre.find_by_name(gets.chomp)
+      if genre
+      genre.songs .each do |song|
+        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+      else
+        puts "genre not found"
+    end
+      end
+      break if choice == "exit"
+    end
+end
 end
